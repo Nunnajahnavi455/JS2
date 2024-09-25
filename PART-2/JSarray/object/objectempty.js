d76ae05b4867c451194fab8be034726c245599c3
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-09-25 18:23:51 +0530

    jsobject

---
+++ PART-2/JSarray/object/objectempty.js
@@ -0,0 +1,11 @@
+let users={
+    uname:"Rahul",
+    uloc:"waynad",
+    avail:"true",
+    email:"rg@gmail.com"
+}
+console.log(user.name)
+console.log(user.length)
+console.log(object.keys(user))
+console.log(object.keys(user).length)
+console.log(object.keys(user).length>0)
