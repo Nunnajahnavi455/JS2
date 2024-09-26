263de102181f53ef27250c2d43e67e495daf4263
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-09-26 18:41:02 +0530

    dom

---
+++ events DOM/events/data2.js
@@ -0,0 +1,6 @@
+function display(){
+    alert("GOOD EVENING")
+}
+function  update(){
+    document.write("GOOD EVENING")
+}
---
+++ events DOM/events/data3.js
@@ -0,0 +1,6 @@
+function changecolor(){
+    // document.getElementById('abc').innerHTML='Test'
+    document.getElementById('abc').style.backgroundColor="green"
+    
+    // alert("Test case")
+}
