51714483930586f9cda2483c4b44c7390bfc4310
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-10-02 18:21:18 +0530

    flat Arrow function

--- PART-2/Flat Arrow functions/seven.js
+++ PART-2/Flat Arrow functions/seven.js
@@ -1,3 +1,10 @@
-//let greet=name => "Hello"+name;   //implicit function
+let greet=name => "Hello"+name;   //implicit function
 let message = greet("Rahul")
 console.log(message)                  //flat Arrow function.
+
+//======================================================================================
+// function greet(name){
+//     return "Hello"+name
+// }                                          //normal function
+// let message= greet("Rahul")
+// console.log(message)
