4013c09dc2da17d377e5403ab2354c77975d9583
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-11-13 19:35:34 +0530

    digital clock

---
+++ PART-2/Events DOM2/digitalclock/data-1.js
@@ -0,0 +1,3 @@
+let ct=new Date().toLocaleTimeString()
+  document.getElementByTagName('span')[0].inner.HTML=ct
+//   document.getElementByTagName('span')[0].inner.HTML="GM"
---
+++ PART-2/Events DOM2/digitalclock/inbuilt.js
@@ -0,0 +1,7 @@
+// setTimeout(function(){
+//     console.log("GM")
+// },4000)
+
+setInterval(function(){
+    console.log("GM")
+},1000)
